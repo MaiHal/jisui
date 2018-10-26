f1c4ed4cf0ff467cc7f58f40a77b9ab23aed22fa
MaiHal <myturn.lovenyc@gmail.com>
2018-10-26 20:57:24 +0900

    一旦コミット

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -14,16 +14,21 @@ class MealsController < ApplicationController
     end
 
     def out_search
-        @meal = Meal.new
-        @categories = recipe_categories['result']['large']
+        @resutaurant = restaurant_search['results']
     end
-
-    
 
     def recipe_categories
         agent = Mechanize.new
         res = agent.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=large&applicationId=1025646104690209174')
         results = JSON.parse(res.body.force_encoding('UTF-8'))
+        return results
+    end
+
+    def restaurant_search
+        agent = Mechanize.new
+        res = agent.get('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?format=json&key=1e1382208c617774&keyword=土間土間 飯田橋東口店')
+        results = JSON.parse(res.body.force_encoding('UTF-8'))
+        puts results['results']['shop'][30]
         return results
     end
 
