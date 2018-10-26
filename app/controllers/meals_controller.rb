cf527c3aa31064d71ad297b3558ce6a9544756f9
MaiHal <myturn.lovenyc@gmail.com>
2018-10-26 19:03:13 +0900

    API叩けた

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,23 @@
+class MealsController < ApplicationController
+    require 'net/http'
+    require 'uri'
+    require 'json'
+    require 'pp'
+    require 'mechanize'
+
+    def search
+        @meal = Meal.new
+        @categories = recipe_categories['result']['large']
+    end
+
+    def recipe_categories
+        agent = Mechanize.new
+        res = agent.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=large&applicationId=1025646104690209174')
+        results = JSON.parse(res.body.force_encoding('UTF-8'))
+        return results
+    end
+
+    def submit
+        redirect_to("/")
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "/mypage/:id" => "users#detail"
   get "/signup" => "users#signup"
   post "/create" => "users#create"
+  get "/meal" => "meals#search"
+  post "/meal/submit" => "meals#submit"
 end
